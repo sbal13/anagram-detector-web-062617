a32fba376eb2b2b06ce48533e93fc07e0459cff9
sbal13 <sbal13@gmail.com>
2017-07-01 10:38:28 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+	attr_accessor :word
+
+	def initialize(word)
+
+		@word = word
+		
+	end
+
+	def match(string_array)
+
+		string_array.select do |test_word|
+			test_word.chars.sort == word.chars.sort
+		end
+
+	end
+
+end
